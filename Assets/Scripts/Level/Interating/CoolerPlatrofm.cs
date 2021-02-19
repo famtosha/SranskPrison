27abfb894101bbc2c29c18f7fc522dc5ea5aa08d
Famtosha <artem_kazakovtsev@mail.ru>
2021-02-19 14:55:04 +0500

    Added moving platforms;

---
+++ Assets/Scripts/Level/Interating/CoolerPlatrofm.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CoolerPlatrofm : MonoBehaviour
+{
+    public Vector3 startPosition;
+    public Vector3 endPosition;
+
+    public float speed;
+
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(startPosition, endPosition, Mathf.PingPong(speed * Time.time, 1));
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(startPosition, transform.localScale);
+        Gizmos.DrawWireCube(endPosition, transform.localScale);
+        Gizmos.DrawLine(startPosition, endPosition);
+    }
+}
