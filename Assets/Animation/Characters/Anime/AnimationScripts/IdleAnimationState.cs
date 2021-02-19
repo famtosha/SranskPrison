d8c5332c849581660fda5aa036ba4a437c2f8213
Famtosha <artem_kazakovtsev@mail.ru>
2021-02-19 14:59:00 +0500

    Added Amine animations;

---
+++ Assets/Animation/Characters/Anime/AnimationScripts/IdleAnimationState.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleAnimationState : StateMachineBehaviour
+{
+    private Rigidbody2D rb;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        rb = animator.gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.01f)
+        {
+            animator.SetBool("IsWalking", true);
+        }
+        if (rb.velocity.y > 0.1f)
+        {
+            animator.SetTrigger("Jump");
+        }
+    }
+}
