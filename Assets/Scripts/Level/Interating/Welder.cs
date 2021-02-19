80cc16104b0287e32ca979c0d17e8e3ec2c897af
Famtosha <artem_kazakovtsev@mail.ru>
2021-02-19 15:25:18 +0500

    Fixed: moving platforms don't move character;

---
+++ Assets/Scripts/Level/Interating/Welder.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Welder : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.gameObject.transform.SetParent(transform);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        collision.gameObject.transform.SetParent(null);
+    }
+}
