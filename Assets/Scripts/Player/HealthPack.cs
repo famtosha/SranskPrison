a24af9bd968d47a3e710689109e4182fb04082ea
Famtosha <artem_kazakovtsev@mail.ru>
2021-02-19 14:51:02 +0500

    Added health pack;

---
+++ Assets/Scripts/Player/HealthPack.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New health pack", menuName = "Custom SO/Items/HealthPack")]
+public class HealthPack : ConsumableItem
+{
+    public int healAmount;
+    public override void Use(GameObject user)
+    {
+        user.GetComponent<Character>().Heal(healAmount);
+    }
+}
