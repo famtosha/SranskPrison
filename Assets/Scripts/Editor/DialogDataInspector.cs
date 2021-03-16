55d372bd5b017f3e545dcde22c41ab0141bddea4
Famtosha <artem_kazakovtsev@mail.ru>
2021-03-16 09:10:43 +0500

    Fixed custom inspector for dialog data doesn't save data;

--- Assets/Scripts/Editor/DialogDataInspector.cs
+++ Assets/Scripts/Editor/DialogDataInspector.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using System.Linq;
-using System.Collections.Generic;
 using System;
 
 [CustomEditor(typeof(DialogData))]
@@ -10,16 +9,25 @@ public class DialogDataInspector : Editor
     public override void OnInspectorGUI()
     {
         var dialogData = target as DialogData;
+        bool isChanged = false;
 
-        var chacaters = Resources.FindObjectsOfTypeAll<DialogCharacter>().ToList();
+        EditorUtility.SetDirty(dialogData);
+        var chacaters = EditorExtention.FindAssetsByType<DialogCharacter>();
         var chacaterNames = chacaters.Select(x => x.characterName).ToArray();
+
+        var prefCharacter = dialogData.character;
         dialogData.character = chacaters[EditorGUILayout.Popup("Character", Mathf.Max(chacaters.IndexOf(dialogData.character), 0), chacaterNames)];
+        isChanged = dialogData.character != prefCharacter;
 
         var sprites = dialogData.character.sprites;
         var faceNames = sprites.Select(x => x.name).ToArray();
 
+        var prefImage = dialogData.image;
         dialogData.image = sprites[EditorGUILayout.Popup("Character face", Mathf.Max(sprites.IndexOf(dialogData.image), 0), faceNames)];
+        isChanged = isChanged || dialogData.image != prefImage;
 
         DrawPropertiesExcluding(serializedObject, new string[] { "character", "image", "m_Script" });
+
+        if (isChanged) AssetDatabase.SaveAssets();
     }
 }
---
+++ Assets/Scripts/Editor/EditorExtention.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using System.Collections.Generic;
+
+public static class EditorExtention
+{
+    public static List<T> FindAssetsByType<T>() where T : UnityEngine.Object
+    {
+        List<T> assets = new List<T>();
+        string[] guids = AssetDatabase.FindAssets(string.Format("t:{0}", typeof(T)));
+        for (int i = 0; i < guids.Length; i++)
+        {
+            string assetPath = AssetDatabase.GUIDToAssetPath(guids[i]);
+            T asset = AssetDatabase.LoadAssetAtPath<T>(assetPath);
+            if (asset != null)
+            {
+                assets.Add(asset);
+            }
+        }
+        return assets;
+    }
+}
